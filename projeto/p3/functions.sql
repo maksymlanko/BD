8ffeea006fc54ad8f4d361779795cbe8a61c40fc
Samuel Barata <oliveira3@icloud.com>
2020-05-17 16:10:56 +0100

    bug function insert_proposta

--- projeto/p3/functions.sql
+++ projeto/p3/functions.sql
@@ -3,16 +3,18 @@ create or replace function insert_proposta(mail varchar(100), fix varchar(500))
 $$
 	declare next_nro int;
  begin
-    select max(nro) into next_nro
-    from proposta_de_correcao group by email
-	WHERE email=mail;
+ 	select max_nro+1 into next_nro
+	from ( 
+		select max(nro) as max_nro, email
+		from proposta_de_correcao
+		group by email
+	) t
+	where email = mail;
 	
     if next_nro is null then next_nro := 1; end if;
 
-    UPDATE proposta_de_correcao
-	SET data_hora=now(), texto=fix
-	WHERE email=mail and nro=next_nro;
+    INSERT INTO proposta_de_correcao (email,nro, data_hora, texto)
+	VALUES (mail, next_nro, now(), fix);
 
     return next_nro;
 end;
-$$ language plpgsql;
