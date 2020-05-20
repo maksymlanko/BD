3408a0de273f8d969ce000fcac0586f8c93f989d
ist189310 <maria.rangel.cunha@tecnico.ulisboa.pt>
2020-05-20 23:36:33 +0100

    strela feito

--- projeto/p3/star_schema.sql
+++ projeto/p3/star_schema.sql
@@ -1,43 +1,44 @@
 DROP TABLE IF EXISTS d_Utilizador;
 create table d_Utilizador(
-	id_Utilizador SERIAL PRIMARY KEY UNIQUE,
-	tipo varchar(30),
+	id_Utilizador SERIAL PRIMARY KEY UNIQUE NOT NULL,
+	tipo varchar(30) NOT NULL,
 	email varchar(60) NOT NULL
 );
 
 DROP TABLE IF EXISTS d_tempo;
 create table d_tempo(
 	id_tempo SERIAL PRIMARY KEY UNIQUE,
-	dia int, 
-	dia_da_semana int, 
-	mes int, 
-	trimestre int, 
+	dia int NOT NULL, 
+	dia_da_semana int NOT NULL, 
+	mes int NOT NULL, 
+	trimestre int NOT NULL, 
 	ano int NOT NULL
 );
 
 DROP TABLE IF EXISTS d_local;
 create table d_local(
-	id_local SERIAL PRIMARY KEY UNIQUE,
-	nome VARCHAR(50),
-	latitude DECIMAL(8,6),
-	longitude DECIMAL(9,6)
+	id_local SERIAL PRIMARY KEY UNIQUE NOT NULL,
+	nome VARCHAR(50) NOT NULL,
+	latitude DECIMAL(8,6) NOT NULL,
+	longitude DECIMAL(9,6) NOT NULL
 );
 
 DROP TABLE IF EXISTS d_lingua;
 create table d_lingua(
-	id_lingua SERIAL PRIMARY KEY UNIQUE, 
+	id_lingua SERIAL PRIMARY KEY UNIQUE NOT NULL, 
 	lingua VARCHAR(30)
 );
 
 
 DROP TABLE IF EXISTS f_anomalia;
 create table f_anomalia( 
-	id_Utilizador int, 
-	id_tempo int, 
-	id_local int, 
-	id_lingua int, 
-	tipo_anomalia varchar(50), 
-	com_proposta boolean,
+	id_Utilizador int NOT NULL, 
+	id_tempo int NOT NULL, 
+	id_local int NOT NULL, 
+	id_lingua int NOT NULL, 
+	tipo_anomalia varchar(50) NOT NULL, 
+	com_proposta boolean NOT NULL,
+	PRIMARY KEY(id_Utilizador, id_tempo, id_local, id_lingua),
 	FOREIGN KEY(id_Utilizador) REFERENCES d_Utilizador(id_Utilizador),
 	FOREIGN KEY(id_tempo) REFERENCES d_tempo(id_tempo),
 	FOREIGN KEY(id_local) REFERENCES d_local(id_local),
--- projeto/p3/star_schema_insert.sql
+++ projeto/p3/star_schema_insert.sql
@@ -1,23 +1,11 @@
 
 
-insert into d_utilizador(email)
-select email
-from Utilizador_Qualificado;
-
-update d_utilizador
-set tipo = 'qualificado'
-where tipo is null;
-
-
-
-insert into d_utilizador(email)
-select email
+insert into d_utilizador(email, tipo)
+select email, 'qualificado'
+from Utilizador_Qualificado
+union
+select email, 'regular'
 from Utilizador_Regular;
-
-update d_utilizador
-set tipo = 'regular'
-where tipo is null;
-
 
 
 CREATE OR REPLACE FUNCTION load_d_tempo()
@@ -60,60 +48,102 @@ insert into d_local(latitude, longitude, nome)
 select latitude, longitude, nome from local_publico;
 
 
-insert into f_anomalia(id_tempo)
-select id_tempo
-from d_tempo;
+insert into f_anomalia(id_utilizador,id_tempo, id_local,id_lingua, tipo_anomalia, com_proposta)
+select id_utilizador, 
+				EXTRACT(YEAR FROM T.ts) * 10000
+				+ EXTRACT(MONTH FROM T.ts)*100
+				+ EXTRACT(DAY FROM T.ts), id_local, id_lingua, 'traducao', false
+from (select anomalia.id as anomalia_id, ts, lingua, lingua2, email_incidencia, email_utilizador,latitude, longitude
+		from anomalia 
+		inner join anomalia_traducao 
+			on anomalia.id = anomalia_traducao.id
+		inner join (select anomalia_id,item_id, email as email_incidencia from incidencia) AS foo 
+			on anomalia.id = foo.anomalia_id
+		inner join (select email as email_utilizador from utilizador) AS fee 
+			on foo.email_incidencia = fee.email_utilizador
+		inner join (select id, latitude, longitude from item) AS faa 
+			on foo.item_id = faa.id) as T
+	left outer join d_lingua d1
+		on d1.lingua = T.lingua
+		or d1.lingua = T.lingua2
+	left outer join d_utilizador d2
+		on d2.email = T.email_incidencia
+	left outer join d_local d3
+		on d3.latitude = T.latitude
+		and d3.longitude = T.longitude
+where T.anomalia_id not in (select anomalia_id from correcao);
 
-insert into f_anomalia(id_utilizador,id_local,id_lingua)
-select id_utilizador, id_local, id_lingua
-from (select ts as data_anomalia, lingua, lingua2, email_incidencia, email_utilizador, email_proposta, latitude, longitude
-from anomalia full outer join anomalia_traducao on anomalia.id = anomalia_traducao.id
-full outer join (select anomalia_id,item_id, email as email_incidencia from incidencia) AS foo on anomalia.id = foo.anomalia_id
-full outer join (select email as email_utilizador from utilizador) AS fee on foo.email_incidencia = fee.email_utilizador
-full outer join (select email as email_proposta from proposta_de_correcao) AS fii on fee.email_utilizador = fii.email_proposta
-full outer join (select id, latitude, longitude from item) AS faa on foo.item_id = faa.id) as T
-	left outer join d_utilizador d1
-		on d1.email = T.email_utilizador
-	left outer join d_local d2
-		on d2.latitude = T.latitude
-		and d2.longitude = T.longitude
-	left outer join d_lingua d3
-		on d3.lingua = T.lingua
-		or d3.lingua = T.lingua2;
+insert into f_anomalia(id_utilizador,id_tempo, id_local,id_lingua, tipo_anomalia, com_proposta)
+select id_utilizador, 
+				EXTRACT(YEAR FROM T.ts) * 10000
+				+ EXTRACT(MONTH FROM T.ts)*100
+				+ EXTRACT(DAY FROM T.ts), id_local, id_lingua, 'redacao', false
+from (select anomalia.id as anomalia_id, ts, lingua, email_incidencia, email_utilizador,latitude, longitude, tem_anomalia_traducao
+		from anomalia 
+		inner join (select anomalia_id,item_id, email as email_incidencia from incidencia) AS foo 
+			on anomalia.id = foo.anomalia_id
+		inner join (select email as email_utilizador from utilizador) AS fee 
+			on foo.email_incidencia = fee.email_utilizador
+		inner join (select id, latitude, longitude from item) AS faa 
+			on foo.item_id = faa.id) as T
+	left outer join d_lingua d1
+		on d1.lingua = T.lingua
+	left outer join d_utilizador d2
+		on d2.email = T.email_incidencia
+	left outer join d_local d3
+		on d3.latitude = T.latitude
+		and d3.longitude = T.longitude
+where tem_anomalia_traducao = false and T.anomalia_id not in (select anomalia_id from correcao);
+
+insert into f_anomalia(id_utilizador,id_tempo, id_local,id_lingua, tipo_anomalia, com_proposta)
+select id_utilizador, 
+				EXTRACT(YEAR FROM T.ts) * 10000
+				+ EXTRACT(MONTH FROM T.ts)*100
+				+ EXTRACT(DAY FROM T.ts), id_local, id_lingua, 'redacao', true
+from (select ts, lingua, email_incidencia, email_utilizador,latitude, longitude, tem_anomalia_traducao
+		from anomalia 
+		inner join (select anomalia_id,item_id, email as email_incidencia from incidencia) AS foo 
+			on anomalia.id = foo.anomalia_id
+		inner join (select email as email_utilizador from utilizador) AS fee 
+			on foo.email_incidencia = fee.email_utilizador
+		inner join (select anomalia_id as id_anomalia_correcao from correcao) AS fii 
+			on anomalia.id = fii.id_anomalia_correcao
+		inner join (select id, latitude, longitude from item) AS faa 
+			on foo.item_id = faa.id) as T
+	left outer join d_lingua d1
+		on d1.lingua = T.lingua
+	left outer join d_utilizador d2
+		on d2.email = T.email_incidencia
+	left outer join d_local d3
+		on d3.latitude = T.latitude
+		and d3.longitude = T.longitude
+where tem_anomalia_traducao = false;
+
+insert into f_anomalia(id_utilizador,id_tempo, id_local,id_lingua, tipo_anomalia, com_proposta)
+select id_utilizador, 
+				EXTRACT(YEAR FROM T.ts) * 10000
+				+ EXTRACT(MONTH FROM T.ts)*100
+				+ EXTRACT(DAY FROM T.ts), id_local, id_lingua, 'traducao', true
+from (select ts, lingua, lingua2, email_incidencia, email_utilizador,latitude, longitude
+		from anomalia 
+		inner join anomalia_traducao 
+			on anomalia.id = anomalia_traducao.id
+		inner join (select anomalia_id,item_id, email as email_incidencia from incidencia) AS foo 
+			on anomalia.id = foo.anomalia_id
+		inner join (select email as email_utilizador from utilizador) AS fee 
+			on foo.email_incidencia = fee.email_utilizador
+		inner join (select anomalia_id as id_anomalia_correcao from correcao) AS fii 
+			on anomalia.id = fii.id_anomalia_correcao	
+		inner join (select id, latitude, longitude from item) AS faa 
+			on foo.item_id = faa.id) as T
+	left outer join d_lingua d1
+		on d1.lingua = T.lingua
+		or d1.lingua = T.lingua2
+	left outer join d_utilizador d2
+		on d2.email = T.email_incidencia
+	left outer join d_local d3
+		on d3.latitude = T.latitude
+		and d3.longitude = T.longitude;
 
 
-
-UPDATE f_anomalia
-SET com_proposta = true
-WHERE id_utilizador in (select id_utilizador from d_utilizador inner join (select ts as data_anomalia, lingua, lingua2, email_incidencia, email_utilizador, email_proposta, latitude, longitude
-from anomalia full outer join anomalia_traducao on anomalia.id = anomalia_traducao.id
-full outer join (select anomalia_id,item_id, email as email_incidencia from incidencia) AS foo on anomalia.id = foo.anomalia_id
-full outer join (select email as email_utilizador from utilizador) AS fee on foo.email_incidencia = fee.email_utilizador
-full outer join (select email as email_proposta from proposta_de_correcao) AS fii on fee.email_utilizador = fii.email_proposta
-full outer join (select id, latitude, longitude from item) AS faa on foo.item_id = faa.id) as T on d_utilizador.email = T.email_proposta); 
-
-UPDATE f_anomalia
-SET com_proposta = true
-WHERE id_tempo in (select id_tempo from f_anomalia inner join (select ts as data_anomalia, lingua, lingua2, email_incidencia, email_utilizador, email_proposta, latitude, longitude
-from anomalia full outer join anomalia_traducao on anomalia.id = anomalia_traducao.id
-full outer join (select anomalia_id,item_id, email as email_incidencia from incidencia) AS foo on anomalia.id = foo.anomalia_id
-full outer join (select email as email_utilizador from utilizador) AS fee on foo.email_incidencia = fee.email_utilizador
-full outer join (select email as email_proposta from proposta_de_correcao) AS fii on fee.email_utilizador = fii.email_proposta
-full outer join (select id, latitude, longitude from item) AS faa on foo.item_id = faa.id) as T on f_anomalia.id_tempo 
-= EXTRACT(YEAR FROM T.data_anomalia) * 10000
-				+ EXTRACT(MONTH FROM T.data_anomalia)*100
-				+ EXTRACT(DAY FROM T.data_anomalia)); 
-
-UPDATE f_anomalia
-SET tipo_anomalia = 'traducao'
-where id_lingua in (select id_lingua from d_lingua inner join (select ts as data_anomalia, lingua, lingua2, email_incidencia, email_utilizador, email_proposta, latitude, longitude, tem_anomalia_traducao
-from anomalia full outer join anomalia_traducao on anomalia.id = anomalia_traducao.id
-full outer join (select anomalia_id,item_id, email as email_incidencia from incidencia) AS foo on anomalia.id = foo.anomalia_id
-full outer join (select email as email_utilizador from utilizador) AS fee on foo.email_incidencia = fee.email_utilizador
-full outer join (select email as email_proposta from proposta_de_correcao) AS fii on fee.email_utilizador = fii.email_proposta
-full outer join (select id, latitude, longitude from item) AS faa on foo.item_id = faa.id) as T on d_lingua.lingua = T.lingua2 and T.tem_anomalia_traducao=true);
-
-UPDATE f_anomalia
-SET tipo_anomalia = 'redacao'
-where id_lingua is not null and tipo_anomalia is null;
 
