541bf83936b2f1c709faef23e27973405c166ab3
Samuel Barata <oliveira3@icloud.com>
2020-05-20 17:52:13 +0100

    queries bem formatadas

--- projeto/p3/queries.sql
+++ projeto/p3/queries.sql
@@ -1,15 +1,19 @@
 --Local publico com menos anomalias registadas
 SELECT l
 FROM incidencia inc
-	JOIN anomalia anom ON inc.anomalia_id = anom.id
-	JOIN item it ON inc.item_id = it.id
+	JOIN anomalia anom 
+        ON inc.anomalia_id = anom.id
+	JOIN item it 
+        ON inc.item_id = it.id
 	NATURAL JOIN local_publico l
 GROUP BY l
 HAVING COUNT(*) <= ALL (
 	SELECT COUNT(*) as cnt
     FROM incidencia inc
-	    JOIN anomalia anom ON inc.anomalia_id = anom.id
-	    JOIN item it ON inc.item_id = it.id
+	    JOIN anomalia anom 
+            ON inc.anomalia_id = anom.id
+	    JOIN item it 
+            ON inc.item_id = it.id
 	    NATURAL JOIN local_publico l
     GROUP BY l
 );
@@ -17,8 +21,10 @@ HAVING COUNT(*) <= ALL (
 --Locais publicos com mais anomalias no primeiro semestre de 2020
 SELECT l.nome, l.latitude, l.longitude
 FROM incidencia inc
-	JOIN anomalia anom ON inc.anomalia_id = anom.id
-	JOIN item it ON inc.item_id = it.id
+	JOIN anomalia anom 
+        ON inc.anomalia_id = anom.id
+	JOIN item it 
+        ON inc.item_id = it.id
 	NATURAL JOIN local_publico l
 WHERE anom.tem_anomalia_traducao is true
 	AND anom.ts BETWEEN '2020-01-01' AND '2020-06-30 23:59:59.999999'
@@ -26,8 +32,10 @@ GROUP BY l.latitude, l.longitude
 HAVING COUNT(*) >= ALL (
 	SELECT COUNT(*)
 	FROM incidencia inc
-	    JOIN anomalia anom ON inc.anomalia_id = anom.id
-	    JOIN item it ON inc.item_id = it.id
+	    JOIN anomalia anom 
+            ON inc.anomalia_id = anom.id
+	    JOIN item it 
+            ON inc.item_id = it.id
 	    NATURAL JOIN local_publico l
 	WHERE anom.tem_anomalia_traducao is true
 	    AND anom.ts BETWEEN '2020-01-01' AND '2020-06-30 23:59:59.999999'
@@ -38,8 +46,10 @@ HAVING COUNT(*) >= ALL (
 SELECT pdc.email
 FROM proposta_de_correcao pdc
     NATURAL JOIN correcao c
-    LEFT JOIN incidencia i ON c.anomalia_id=i.anomalia_id
-    LEFT JOIN item it on i.item_id=it.id
+    LEFT JOIN incidencia i 
+        ON c.anomalia_id=i.anomalia_id
+    LEFT JOIN item it 
+        ON i.item_id=it.id
 WHERE it.latitude < 39.336775 
     AND pdc.data_hora BETWEEN '2020-01-01 00:00:00' AND '2020-12-31 23:59:59.999999'
 GROUP BY pdc.email;
@@ -50,9 +60,12 @@ GROUP BY pdc.email;
 SELECT pdc.email
 FROM proposta_de_correcao pdc
     NATURAL JOIN correcao c
-    LEFT JOIN incidencia i ON c.anomalia_id=i.anomalia_id
-    LEFT JOIN utilizador u ON i.email=u.email
-    LEFT JOIN anomalia a ON a.id=i.anomalia_id
+    LEFT JOIN incidencia i 
+        ON c.anomalia_id=i.anomalia_id
+    LEFT JOIN utilizador u 
+        ON i.email=u.email
+    LEFT JOIN anomalia a 
+        ON a.id=i.anomalia_id
 WHERE ts BETWEEN '2020-01-01' AND '2020-12-31 23:59:59.999999'
     AND pdc.email = u.email
 GROUP BY pdc.email
@@ -62,9 +75,12 @@ EXCEPT
 SELECT pdc.email
 FROM proposta_de_correcao pdc
     NATURAL JOIN correcao c
-    LEFT JOIN incidencia i ON c.anomalia_id=i.anomalia_id
-    LEFT JOIN utilizador u ON i.email=u.email
-    LEFT JOIN anomalia a ON a.id=i.anomalia_id
+    LEFT JOIN incidencia i 
+        ON c.anomalia_id=i.anomalia_id
+    LEFT JOIN utilizador u 
+        ON i.email=u.email
+    LEFT JOIN anomalia a 
+        ON a.id=i.anomalia_id
 WHERE ts BETWEEN '2020-01-01' AND '2020-12-31 23:59:59.999999'
     AND pdc.email <> u.email
 GROUP BY pdc.email;
